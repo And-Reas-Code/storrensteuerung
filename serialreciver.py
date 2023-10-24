fcd056e07c1fe15fb8566d32b38cdba989370ab1
Andreas <sueppi@gmx.ch>
2023-10-24 21:38:21 +0200

    Path

--- serialreciver.py
+++ serialreciver.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
     ser.reset_input_buffer()
 
-    init_file_switch = open("/root/scripts/steuerung/status/switches.txt", "wb")
+    init_file_switch = open("/root/storrensteuerung/status/switches.txt", "wb")
     # Values: [ WIND-RAIN-AUTO, SUN-AUTO ] 
     init_states = ["TRUE", "TRUE"] 
     pickle.dump(init_states, init_file_switch) 
@@ -22,17 +22,17 @@ if __name__ == '__main__':
             wind = int(serial_states[1])
             sun = int(serial_states[2])
 
-            file = open("/root/scripts/steuerung/status/variables.txt", "wb")
+            file = open("/root/storrensteuerung/status/variables.txt", "wb")
             pickle.dump(serial_states, file) 
             file.flush()
             file.close()
 
-            file_switch = open("/root/scripts/steuerung/status/switches.txt", "rb")
+            file_switch = open("/root/storrensteuerung/status/switches.txt", "rb")
             switch = pickle.load(file_switch) 
             wind_rain_auto = switch[0].__eq__('TRUE')
             sun_auto = switch[1].__eq__('TRUE')
 
-            # file2 = open("/root/scripts/steuerung/status/variables.txt", 'rb')
+            # file2 = open("/root/storrensteuerung/status/variables.txt", 'rb')
             # mya = pickle.load(file2) 
             # print(mya[0])
             # print(mya[1])
