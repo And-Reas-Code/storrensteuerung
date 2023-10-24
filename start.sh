124f257336487a62e427fe8f458c2e47b61a4ea8
Andreas <sueppi@gmx.ch>
2023-10-24 21:22:44 +0200

    path moved

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@
 sudo nohup python3 websocket.py > /home/pi/logfile-websocket.out 2>&1 &
 
 # Finally, I record the PID of the process before the script exits.
-sudo echo $! > /root/scripts/steuerung/status/www.pid
+sudo echo $! > /root/storrensteuerung/status/www.pid
