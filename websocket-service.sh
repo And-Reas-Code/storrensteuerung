4ba45366e2344623943ad0897412d8f534f14fed
Andreas <sueppi@gmx.ch>
2025-01-06 21:21:33 +0100

    korr

--- websocket-service.sh
+++ websocket-service.sh
@@ -5,7 +5,7 @@
 
 # Now I want to invoke the process in the background and redirect
 # all of the output to a log file.
-python3 websocket.py > /home/pi/logfile-websocket.out 2>&1 &
+/user/bin/python3 /root/storrensteuerung/websocket.py > /home/pi/logfile-websocket.out 2>&1 &
 
 # Finally, I record the PID of the process before the script exits.
 echo $! > /root/storrensteuerung/status/www.pid
