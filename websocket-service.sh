cf897f6bc9248850768af592caf8d0b2f70a9e51
Andreas <sueppi@gmx.ch>
2025-01-06 21:29:23 +0100

    fehler

--- websocket-service.sh
+++ websocket-service.sh
@@ -5,7 +5,7 @@
 
 # Now I want to invoke the process in the background and redirect
 # all of the output to a log file.
-/user/bin/python3 /root/storrensteuerung/websocket.py > /home/pi/logfile-websocket.out 2>&1 &
+/usr/bin/python3 /root/storrensteuerung/websocket.py > /home/pi/logfile-websocket.out 2>&1 &
 
 # Finally, I record the PID of the process before the script exits.
 echo $! > /root/storrensteuerung/status/www.pid
